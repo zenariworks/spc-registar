4e65546ff0d3b35021b1b064c677c2e817b7e8a2
Milan Jelisavcic <5294258+milanjelisavcic@users.noreply.github.com>
2023-10-14 12:10:39 +0200

    Додат приказ и штампа за свештеника

--- crkva/crkva/urls.py
+++ crkva/crkva/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('registar.urls')),
 ]
 
 if settings.DEBUG:
