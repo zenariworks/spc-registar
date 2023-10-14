21cc325ad56086f03bf05732c8574b7c348fed6e
Milan Jelisavcic <5294258+milanjelisavcic@users.noreply.github.com>
2023-10-14 12:32:23 +0200

    Додата штампа и приказ за свештеника

--- crkva/registar/urls.py
+++ crkva/registar/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
+from .views import SvestenikView, SvestenikPDF
 
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('svestenik/<int:sv_rbr>/', views.svestenik_detail, name='svestenik_detail'),
-    path('svestenik/print/<int:pk>/', views.SvestenikPDFView.as_view(), name='svestenik_pdf'),
+    path('svestenik/<int:sv_rbr>/', SvestenikView.as_view(), name='svestenik_detail'),
+    path('svestenik/print/<int:pk>/', SvestenikPDF.as_view(), name='svestenik_pdf'),
 ]
