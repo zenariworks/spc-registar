0569c01f080752d235875e71e5f0f6cd5a031c37
Milan Jelisavcic <5294258+milanjelisavcic@users.noreply.github.com>
2023-10-16 13:42:30 +0200

    Add init template for krstenja

--- crkva/registar/urls.py
+++ crkva/registar/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 from .views import SvestenikView, SvestenikPDF
+from .views import KrstenjeView, KrstenjePDF
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('svestenik/<int:sv_rbr>/', SvestenikView.as_view(), name='svestenik_detail'),
     path('svestenik/print/<int:pk>/', SvestenikPDF.as_view(), name='svestenik_pdf'),
+    path('krstenje/<int:k_rbr>/', KrstenjeView.as_view(), name='krstenje_detail'),
+    path('krstenje/print/<int:pk>/', KrstenjePDF.as_view(), name='krstenje_pdf'),
 ]
