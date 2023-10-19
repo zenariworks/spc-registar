8560a59e119fb6df7a0ac5430652720001dc978e
Milan Jelisavcic <5294258+milanjelisavcic@users.noreply.github.com>
2023-10-19 20:20:03 +0200

    Додат унос слава

--- crkva/registar/admin/slava_admin.py
+++ crkva/registar/admin/slava_admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from import_export.admin import ImportExportMixin
 
 
 class SlavaAdmin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ("uid", "назив")
-    ordering = ("uid",)
+    list_display = ("dan", "mesec", "opsti_naziv", "naziv", )
+    ordering = ("mesec", "dan", )
+    list_display_links = ("naziv", )
