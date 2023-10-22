b12aa94d4469e1edb466c5bc7bd88d8494392e4b
Milan Jelisavcic <5294258+milanjelisavcic@users.noreply.github.com>
2023-10-22 18:44:15 +0200

    Remove extra models

--- crkva/registar/admin/__init__.py
+++ crkva/registar/admin/__init__.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 
 from registar.models.domacinstvo import Domacinstvo
-from registar.models.error import Error
 from registar.models.krstenje import Krstenje
-from registar.models.menu import Menu
-from registar.models.password import Password
 from registar.models.osoba import Osoba
 from registar.models.slava import Slava
 from registar.models.svestenik import Svestenik
@@ -23,10 +20,7 @@ from .ulica_admin import UlicaAdmin
 
 # Register your models here.
 admin.site.register(Domacinstvo, DomacinstvoAdmin)
-admin.site.register(Error)
 admin.site.register(Krstenje, KrstenjeAdmin)
-admin.site.register(Menu)
-admin.site.register(Password)
 admin.site.register(Osoba)
 admin.site.register(Slava, SlavaAdmin)
 admin.site.register(Svestenik, SvestenikAdmin)
