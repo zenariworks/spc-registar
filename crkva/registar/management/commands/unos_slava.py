a4e065d07132fac182a180aefdd7919caa73e666
Milan Jelisavcic <5294258+milanjelisavcic@users.noreply.github.com>
2023-10-23 22:32:02 +0200

    Додат унос занимања

--- crkva/registar/management/commands/unos_slava.py
+++ crkva/registar/management/commands/unos_slava.py
@@ -4,29 +4,6 @@ from registar.models import Dan
 from registar.models import Mesec
 from registar.models import Slava
 
-# Loading data from the txt file into raw_data
-with open("slave.sql", "r", encoding="utf-8") as file:
-    raw_data = [line.strip() for line in file]
-
-parsed_data = []
-
-# Parsing each entry
-for entry in raw_data:
-    parts = entry.split(";")
-
-    # If there are three parts, then there's an optional name
-    if len(parts) == 3:
-        name, opsti_naziv, date_part = parts
-    elif len(parts) == 2:
-        name, date_part = parts
-        opsti_naziv = None  # or some default value
-    else:
-        # Handle other cases or raise an error
-        continue
-
-    day, month = map(int, date_part.split(","))
-    parsed_data.append((name, opsti_naziv, day, month))
-
 
 # Populate the Django model
 class Command(BaseCommand):
@@ -45,3 +22,23 @@ class Command(BaseCommand):
                 dan=dan_instance,
                 mesec=mesec_instance,
             )
+
+    def _parse_data(self):
+        with open("slave.sql", "r", encoding="utf-8") as file:
+            raw_data = [line.strip() for line in file]
+
+        parsed_data = []
+        for entry in raw_data:
+            parts = entry.split(";")
+
+            if len(parts) == 3:
+                name, opsti_naziv, date_part = parts
+            elif len(parts) == 2:
+                name, date_part = parts
+                opsti_naziv = None  # or some default value
+            else:
+                # Handle other cases or raise an error
+                continue
+
+            day, month = map(int, date_part.split(","))
+            parsed_data.append((name, opsti_naziv, day, month))
