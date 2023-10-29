dd8e657a079a3d50e8eed6a29682a6a48f7548c4
Milan Jelisavcic <5294258+milanjelisavcic@users.noreply.github.com>
2023-10-29 16:12:56 +0100

    Update base.html

--- crkva/registar/urls.py
+++ crkva/registar/urls.py
@@ -1,23 +1,20 @@
 from django.urls import path
 
 from . import views
-from .views import SvestenikView, SvestenikPDF
-from .views import KrstenjeView, KrstenjePDF, KrstenjaListView
-from .views import OsobaView, OsobeListView, OsobaPDF
 
 
 urlpatterns = [
     path("", views.index, name="pocetna"),
 
-    path("osobe/", OsobeListView.as_view(), name="osobe"),
-    path("osoba/<uuid:uid>/", OsobaView.as_view(), name="osoba_detail"),
-    path("osoba/print/<uuid:uid>/", OsobaPDF.as_view(), name="osoba_pdf"),
+    path("osobe/", views.OsobeList.as_view(), name="osobe"),
+    path("osoba/<uuid:uid>/", views.OsobaView.as_view(), name="osoba_detail"),
+    path("osoba/print/<uuid:uid>/", views.OsobaPDF.as_view(), name="osoba_pdf"),
 
+    path('krstenja/', views.KrstenjaList.as_view(), name='krstenja'),
+    path("krstenje/<uuid:uid>/", views.KrstenjeView.as_view(), name="krstenje_detail"),
+    path("krstenje/print/<uuid:uid>/", views.KrstenjePDF.as_view(), name="krstenje_pdf"),
 
-    path('krstenja/', KrstenjaListView.as_view(), name='krstenja'),
-    path("krstenje/<uuid:uid>/", KrstenjeView.as_view(), name="krstenje_detail"),
-    path("krstenje/print/<uuid:uid>/", KrstenjePDF.as_view(), name="krstenje_pdf"),
-
-    path("svestenik/<uuid:uid>/", SvestenikView.as_view(), name="svestenik_detail"),
-    path("svestenik/print/<uuid:uid>/", SvestenikPDF.as_view(), name="svestenik_pdf"),
+    path("svestenici/", views.SvesteniciList.as_view(), name="svestenici"),
+    path("svestenik/<uuid:uid>/", views.SvestenikView.as_view(), name="svestenik_detail"),
+    path("svestenik/print/<uuid:uid>/", views.SvestenikPDF.as_view(), name="svestenik_pdf"),
 ]
