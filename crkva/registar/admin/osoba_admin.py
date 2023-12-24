276f4b2228b6809c747eafaa7f997f9ae74085af
Milan Jelisavcic <5294258+milanjelisavcic@users.noreply.github.com>
2023-12-24 23:40:59 +0100

    Update admin views

--- crkva/registar/admin/osoba_admin.py
+++ crkva/registar/admin/osoba_admin.py
@@ -9,8 +9,16 @@ class ZanimanjeFilter(AutocompleteFilter):
 
 
 class OsobaAdmin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ("ime", "prezime", "zanimanje")
-    ordering = ("uid",)
-    autocomplete_fields = ["zanimanje__naziv"]
-    search_fields = ["zanimanje__naziv"]
+    list_display = ("get_full_name", "datum_rodjenja", "get_zanimanje_naziv")
+    ordering = ("datum_rodjenja",)
+    autocomplete_fields = ["zanimanje"]
+    search_fields = ["ime", "prezime", "zanimanje__naziv"]
     list_filter = [ZanimanjeFilter]
+
+    def get_full_name(self, obj):
+        return f"{obj.ime} {obj.prezime}"
+    get_full_name.short_description = 'Име и Презиме'
+
+    def get_zanimanje_naziv(self, obj):
+        return obj.zanimanje.naziv if obj.zanimanje else ''
+    get_zanimanje_naziv.short_description = "Занимање"
