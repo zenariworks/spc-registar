9169e841e05b731338934da666522eca204fae3f
Milan Jelisavcic <5294258+milanjelisavcic@users.noreply.github.com>
2023-12-24 23:56:43 +0100

    Format admina

--- crkva/registar/admin/krstenje_admin.py
+++ crkva/registar/admin/krstenje_admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from import_export.admin import ImportExportMixin
 
 
--- crkva/registar/admin/svestenik_admin.py
+++ crkva/registar/admin/svestenik_admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
+
 from import_export.admin import ImportExportMixin
 
 
 class SvestenikAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ("get_full_name", "zvanje", "parohija")
-    ordering = ("osoba__ime", "osoba__prezime")
+    ordering = ("osoba__prezime", "osoba__ime")
 
     def get_full_name(self, obj):
         return f"{obj.osoba.ime} {obj.osoba.prezime}"
-    get_full_name.short_description = 'Име и Презиме'
+    get_full_name.short_description = 'Име и презиме'
     get_full_name.admin_order_field = 'osoba__ime'
