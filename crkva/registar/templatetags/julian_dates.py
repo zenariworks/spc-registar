eae9c5eb96b5fa833873e8ace169ab2ef8a6b726
Milan Jelisavcic <5294258+milanjelisavcic@users.noreply.github.com>
2024-08-13 23:08:47 +0200

    Manje izmene

--- crkva/registar/templatetags/julian_dates.py
+++ crkva/registar/templatetags/julian_dates.py
@@ -1,30 +1,47 @@
+"""
+Модул филтера за конверзију грегоријанских датума у јулијанске
+који се користи у Ђанго шаблонима.
+"""
+
 import datetime
 
 from django import template
-
-from crkva.registar.utils import GREGORIAN_MONTHS
+from registar.utils import GREGORIAN_MONTHS
 
 register = template.Library()
 
 
-# Add a function to convert Gregorian date to Julian date
 def gregorian_to_julian(date):
-    """Конверзија грегоријанског датума у јулијански."""
+    """
+    Конверзија грегоријанског датума у јулијански.
+
+    :param date: грегоријански датум
+    :type date: datetime.date
+    :return: јулијански датум
+    :rtype: datetime.date
+    """
     julian_date = date - datetime.timedelta(days=13)
     return julian_date
 
 
-def to_julian_date(value):
-    """Конверзија датума у јулијански."""
-    if isinstance(value, datetime.date):
-        gregorian_month = GREGORIAN_MONTHS[value.month]
-        julian_date = gregorian_to_julian(value)
+def to_julian_date(datum):
+    """
+    Конверзија датума у јулијански.
+
+    :param datum: датум за конверзију
+    :type datum: datetime.date
+    :return: датум у формату "година, месец дан. (јулијански дан.)"
+    :rtype: str
+    """
+    if isinstance(datum, datetime.date):
+        gregorian_month = GREGORIAN_MONTHS[datum.month]
+        julian_date = gregorian_to_julian(datum)
         julian_month = GREGORIAN_MONTHS[julian_date.month]
 
         if gregorian_month == julian_month:
-            return f"{value.year}, {gregorian_month} {value.day}. ({julian_date.day}.)"
+            return f"{datum.year}, {gregorian_month} {datum.day}. ({julian_date.day}.)"
         else:
-            return f"{value.year}, {gregorian_month} {value.day}. ({julian_month} {julian_date.day}.)"
+            return f"{datum.year}, {gregorian_month} {datum.day}. ({julian_month} {julian_date.day}.)"
     return ""
 
 
