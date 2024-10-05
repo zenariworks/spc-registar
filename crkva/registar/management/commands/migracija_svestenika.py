4943bdb1e7d7a2b15b76c69158d5ad141485541b
MCMM\sasa.ivancev <sasa.ivancev@united.cloud>
2024-10-06 00:14:14 +0200

    zavrsena migracija tabele slava i svestenika. wip - tabela krstenja

---
+++ crkva/registar/management/commands/migracija_svestenika.py
@@ -0,0 +1,94 @@
+"""
+Migracija tabele 'HSPSVEST.sqlite' u tabelu 'svestenici' (tabela svestenika)
+"""
+
+from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
+from registar.models import Parohija, Svestenik
+from registar.management.commands.random_utils import RandomUtils
+from registar.models.svestenik import zvanja
+import random
+from datetime import date, datetime, time, timedelta
+import sqlite3
+
+class Command(BaseCommand):
+    """
+    Класа Ђанго команде за попуњавање табеле svestenika
+
+    cmd:
+    docker compose run --rm app sh -c "python manage.py migracija_svestenika"
+    """
+
+    help = "'HSPSVEST.sqlite' u tabelu 'svestenici'"
+
+    def handle(self, *args, **kwargs):
+        parsed_data = self._parse_data()
+        created_count = 0
+
+        for ime_prezime, zvanje, parohija, datum_rodjenja in parsed_data:
+            try:
+                broj_parohije = self._convert_roman_to_integer(parohija)
+                parohija_instance, _ = Parohija.objects.get_or_create(naziv=broj_parohije)
+
+                # ako datum rodjenja nije poznat, postavi na danasnji datum
+                if datum_rodjenja == None:
+                    datum_rodjenja = date.today()
+                    #datum_rodjenja = RandomUtils.random_date_of_birth(25)
+                #print(datum_rodjenja)
+
+                svestenik = Svestenik(
+                    ime=ime_prezime,
+                    prezime=ime_prezime,
+                    mesto_rodjenja="",
+                    datum_rodjenja=datum_rodjenja,
+                    zvanje=zvanje,
+                    parohija=parohija_instance,
+                )
+                svestenik.save()
+
+                created_count += 1
+
+            except IntegrityError as e:
+                self.stdout.write(self.style.ERROR(f"Грешка при креирању уноса: {e}"))
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Успешно попуњена табела 'svestenici': {created_count} нових уноса."
+            )
+        )
+
+    def _convert_roman_to_integer(self, roman_numeral):
+        # Define a mapping from Roman numerals to integers
+        roman_to_int = {
+            'I': '1',
+            'II': '2',
+            'III': '3',
+            '1': '1',
+            '2': '2',
+            '3': '3'
+        }
+
+        # Convert each Roman numeral to its integer value
+        #converted_value = roman_to_int.get(roman_numeral, None)
+        
+        # vrati '0' za broj parohije ako svesteniku nije dodeljena parohija koja ima redni broj [1,2,3]
+        converted_value = roman_to_int.get(roman_numeral, '0')
+        return converted_value
+
+    def _parse_data(self):
+        """
+        Migrira tabelu HSPSVEST.sqlite
+        :return: Листа парсираних података (ime_prezime, zvanje, parohija, datum_rodjenja)
+        """
+        parsed_data = []
+        with sqlite3.connect("fixtures/combined_original_hsp_database.sqlite") as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT sv_ime, sv_zvanje, sv_paroh, sv_datrod FROM HSPSVEST")
+            rows = cursor.fetchall()
+
+            for row in rows:
+                ime_prezime, zvanje, parohija, datum_rodjenja = row
+                parsed_data.append((ime_prezime, zvanje, parohija, datum_rodjenja))
+
+        return parsed_data
+
