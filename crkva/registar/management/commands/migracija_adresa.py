a5d181182763f956f52568a4e05946a153455e3e
MCMM\sasa.ivancev <sasa.ivancev@united.cloud>
2024-10-06 19:39:37 +0200

    wip - migracija adresa

---
+++ crkva/registar/management/commands/migracija_adresa.py
@@ -0,0 +1,112 @@
+"""
+Migracija tabele 'HSPDOMACINI.sqlite' u tabele 'opstine', 'mesta', 'ulice', 'adrese'
+"""
+
+from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
+from registar.models import Parohija, Svestenik
+from registar.models.opstina import Opstina
+from registar.models import Mesto
+from registar.models.svestenik import zvanja
+import random
+from datetime import date, datetime, time, timedelta
+import sqlite3
+
+class Command(BaseCommand):
+    """
+    Класа Ђанго команде за попуњавање табеле 'opstine', 'mesta', 'ulice', 'adrese'
+
+    cmd:
+    docker compose run --rm app sh -c "python manage.py migracija_adresa"
+    """
+
+    help = "'HSPDOMACINI.sqlite' u tabele 'opstine', 'mesta', 'ulice', 'adrese'"
+
+    def handle(self, *args, **kwargs):
+
+        # tabela 'opstine': Cukarica, Srbija
+        opstina = Opstina.objects.get_or_create(naziv="Cukarica")
+
+        # tabela 'mesta': Cukarica
+        mesta =  Mesto.objects.get_or_create(naziv="Cukarica")
+
+
+        # parsed_data = self._parse_data()
+        # created_count = 0
+
+        # for ime_prezime, zvanje, parohija, datum_rodjenja in parsed_data:
+        #     try:
+        #         broj_parohije = self._convert_roman_to_integer(parohija)
+        #         parohija_instance, _ = Parohija.objects.get_or_create(naziv=broj_parohije)
+
+        #         # razdvoji ime i prezime" "ime prezime" -> ["ime", "prezime"]
+        #         # i unesi kao ime i prezime
+        #         ime_prezime = ime_prezime.split(" ")
+
+        #         # ako datum rodjenja nije poznat, postavi na danasnji datum
+        #         if datum_rodjenja == None:
+        #             datum_rodjenja = date.today()
+        #             #datum_rodjenja = RandomUtils.random_date_of_birth(25)
+        #         #print(datum_rodjenja)
+
+        #         svestenik = Svestenik(
+        #             ime=ime_prezime[0],
+        #             prezime=ime_prezime[1],
+        #             mesto_rodjenja="",
+        #             datum_rodjenja=datum_rodjenja,
+        #             zvanje=zvanje,
+        #             parohija=parohija_instance,
+        #         )
+        #         svestenik.save()
+
+        #         created_count += 1
+
+        #     except IntegrityError as e:
+        #         self.stdout.write(self.style.ERROR(f"Грешка при креирању уноса: {e}"))
+
+        # self.stdout.write(
+        #     self.style.SUCCESS(
+        #         f"Успешно попуњена табела 'svestenici': {created_count} нових уноса."
+        #     )
+        # )
+
+    # def _convert_roman_to_integer(self, parohija):
+    #     # Define a mapping from Roman numerals to integers
+    #     roman_to_int = {
+    #         'I': '1',
+    #         'II': '2',
+    #         'III': '3',
+    #         '1': '1',
+    #         '2': '2',
+    #         '3': '3'
+    #     }
+
+    #     # remove spaces from the end of the string
+    #     parohija = parohija.rstrip()
+    #     #print("roman_numeral: " + parohija)
+
+    #     # Convert each Roman numeral to its integer value
+    #     #converted_value = roman_to_int.get(roman_numeral, None)
+        
+    #     # vrati '0' za broj parohije ako svesteniku nije dodeljena parohija koja ima redni broj [1,2,3]
+    #     converted_value = roman_to_int.get(parohija, '')
+    #     #print("broj_parohije: " + converted_value)
+    #     return converted_value
+
+    def _parse_data(self):
+        """
+        Migracija tabele 'HSPDOMACINI.sqlite' u tabele 'opstine', 'mesta', 'ulice', 'adrese'
+        :return: Листа парсираних података (ime_prezime, zvanje, parohija, datum_rodjenja)
+        """
+        parsed_data = []
+        with sqlite3.connect("fixtures/combined_original_hsp_database.sqlite") as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT ul_sifra, ul_naziv, ul_rbrsv FROM HSPULICE")
+            rows = cursor.fetchall()
+
+            for row in rows:
+                sifra_ulice, naziv_ulice, ime_prezime, zvanje, parohija, datum_rodjenja = row
+                parsed_data.append((ime_prezime, zvanje, parohija, datum_rodjenja))
+
+        return parsed_data
+
