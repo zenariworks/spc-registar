c9a53f2db8a44386cf64c42636aab45c4c1e263f
MCMM\sasa.ivancev <sasa.ivancev@united.cloud>
2024-10-06 23:20:25 +0200

    - kreirana fja za konvert latinice i spec znaka u cirilicu - promenili smo model Svestenik tako da datum_rodjenja ne mora da bude popunjen (tako je i u staroj bazi, a taj podatak se inace nigde ne koristi i prirodno je da bude opcion) - promenili smo da uid za Svestenika ne bude uuid nego svestenik_id iz stare baze, da bi pojednostavili migraciju ulica - sredjena komanda za migraciju svestenika u vezi sa tim - zavrsena migracija_ulica - ona popunjava tabelu opstina, mesto, ulica (u njoj poveze svestenik_id koji je nadlezan za nju) - dodata recreate-db.sh skripta koja olaksava development

---
+++ recreate-db.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+usage() {
+    echo -e "\e[1;33m\n Re-create databaase 'crkva-db-1' ( for dev purposes only ) \e[0m"
+    echo "
+ 
+ When database is re-created, all data is lost.
+ It is necessary to recreate database when models are changed, 
+ file E:\projects\hram-svete-petke\crkva\crkva\registar\migrations\0001_initial.py 
+
+ Run this script only in development environment!
+ 
+ usage:
+ "
+ echo -e "\e[1;33m    ./start.sh -h|--help         - print usage\e[0m"
+ echo ""
+ exit
+}
+
+#set -x
+
+
+function recreate_database(){
+    
+    # create database image: postgres:13-alpine
+    docker compose run --rm app sh -c "python manage.py makemigrations && python manage.py migrate"
+    
+    # import test data (comment the following 3 lines in final migration script)
+    # docker compose run --rm app sh -c "python manage.py unosi"
+    # docker compose run --rm app sh -c "python manage.py unos_krstenja"
+    # docker compose run --rm app sh -c "python manage.py unos_vencanja"
+
+    docker compose run --rm app sh -c "python manage.py migracija_slava"
+    docker compose run --rm app sh -c "python manage.py migracija_svestenika"
+    docker compose run --rm app sh -c "python manage.py unos_drzava"
+    docker compose run --rm app sh -c "python manage.py migracija_ulica"
+
+    # create super user, default uid/pwd: app/svpetka
+    # NOTE: for email just press Enter...
+    # docker compose run --rm app sh -c "python manage.py createsuperuser"
+
+    # run app
+    # docker compose up 
+
+}
+readonly -f recreate_database
+
+#run script
+recreate_database
