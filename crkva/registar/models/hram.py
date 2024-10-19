3ec26590a03fd5e9d13e7365a42dfbb75c12f3a8
Milan Jelisavcic <5294258+zenfiric@users.noreply.github.com>
2024-10-19 23:39:10 +0200

    Dosta izmena na sredjivanju stampe

--- crkva/registar/models/hram.py
+++ crkva/registar/models/hram.py
@@ -1,21 +1,14 @@
 """Модул модела храма у бази података."""
-
 import uuid
 
 from django.db import models
 
-from .adresa import Adresa
-#from .parohija import Parohija
-
 
 class Hram(models.Model):
     """Класа која представља храмове."""
-
     uid = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
 
     naziv = models.CharField(max_length=200, verbose_name="назив")
-    #adresa = models.ForeignKey(Adresa, on_delete=models.SET_NULL, null=True)
-    #parohija = models.ManyToManyField(Parohija, verbose_name="парохије")
 
     def __str__(self):
         return self.naziv
