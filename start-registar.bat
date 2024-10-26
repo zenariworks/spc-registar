9c25a99f2c0d33c8f99518688f54f6b6111f62c1
WINCTRL-QFMDB86\svpetka <sasa.ivancev@gmail.com>
2024-10-27 00:28:43 +0200

    start-registart.bat fixed to start browser automatically

--- start-registar.bat
+++ start-registar.bat
@@ -6,5 +6,10 @@ REM tako da sve jednim klikom bude spremno za stampu
 
 REM Start WSL, navigate to the directory, run start.sh, wait for 15 seconds, and open the browser
 wsl --shutdown
-wsl -e bash -c "source /home/sasa/.python_venv/bin/activate &&  cd /home/sasa/crkva && ./start.sh > start_log.txt && sleep 15 && powershell.exe -c start 'http://localhost:8000'"
-wsl --list --running
+
+REM wsl -e bash -c "source /home/sasa/.python_venv/bin/activate &&  cd /home/sasa/crkva && ./start.sh  && sleep 15 && powershell.exe -c start 'http://localhost:8000'"
+powershell.exe -c start 'http://localhost:8000'
+
+REM "Osvezi tab 'http://localhost:8000' u browser-u za 15 sekundi..."
+wsl -e bash -c "source /home/sasa/.python_venv/bin/activate &&  cd /home/sasa/crkva && ./start.sh"
+REM wsl --list --running
