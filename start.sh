99a24aa301faf25091770d5d77654216d7d03d65
sasa <sasa.ivancev@gmail.com>
2025-01-24 21:37:38 +0100

    fix mesto rodjenja deteta kod stampe krstenica

--- start.sh
+++ start.sh
@@ -6,11 +6,27 @@
 
 #set -x
 
-# WSL setup (crkva)
-python migrate-original-dbf-files-to-sqlite.py --src_dir "/mnt/c/HramSP/dbf" --dest_dir "crkva/fixtures"
+# Default location is church [-c]. Use '-h' for 'home' setup
 
-# WSL setup (home)
-#python migrate-original-dbf-files-to-sqlite.py --src_dir "/mnt/e/projects/hram-svete-petke/resources/dbf" --dest_dir "crkva/fixtures"
+location="c"
+
+# Parse command line arguments
+while getopts "hc" opt; do
+ case $opt in
+   h) location="h" ;;
+   c) location="c" ;;
+   *) echo "Usage: $0 [-h] [-c]"; exit 1 ;;
+ esac
+done
+
+if [ "$location" = "h" ]; then
+  # WSL setup (home)
+  python migrate-original-dbf-files-to-sqlite.py --src_dir "/mnt/e/projects/hram-svete-petke/resources/dbf" --dest_dir "crkva/fixtures" 
+else
+  # WSL setup (crkva)
+  python migrate-original-dbf-files-to-sqlite.py --src_dir "/mnt/c/HramSP/dbf" --dest_dir "crkva/fixtures"
+fi
+
 
 docker compose run --rm app sh -c "python manage.py migracija_krstenja"
 docker compose run --rm app sh -c "python manage.py migracija_vencanja"
