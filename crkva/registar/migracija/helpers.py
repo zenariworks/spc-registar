cdd99aa676842f961bab3786fafc83df5ad2ca8b
Milan Jelisavcic <5294258+zenfiric@users.noreply.github.com>
2026-06-14 09:21:22 +0000

    refactor(registar): обједини Konvertor на utils, уклони convert_utils и празан resources (#300)
    Постојале су две класе `Konvertor`: груба у `management/commands/convert_utils.py`
    (штампа упозорења, без обраде `None`, без докстрингова) и дотерана у
    `utils/migration_converters.py` (логовање, обрада `None`/`TypeError`,
    докстрингови, плюс `date`/`split_name`). У продукцији је била коришћена
    лошија копија, а дотерана само у сопственом тесту.
    
    Уз то, `convert_utils.py` је стајао у `commands/` иако нема `Command`
    класу (Django би пукао при покушају извршавања), а пакет `registar/resources/`
    је био празан и неискоришћен.
    
    - 3 увоза (`migracija_svestenika`, `migracija_ulice_svestenik`,
      `migracija/helpers`) препоинтирана на `registar.utils.migration_converters`
      (исти `string`/`int` API, само бољи)
    - обрисан `management/commands/convert_utils.py`
    - обрисан празан пакет `registar/resources/`

--- crkva/registar/migracija/helpers.py
+++ crkva/registar/migracija/helpers.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import re
 from datetime import date, time
 
-from registar.management.commands.convert_utils import Konvertor
+from registar.utils.migration_converters import Konvertor
 
 # --- Презиме (surname) ---
 
